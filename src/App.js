b33210f9cec86f9290595c1dad69148561d09fc3
aditya <aditya.sharma9827.as@gmail.com>
2021-08-07 14:32:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useEffect, useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+    const [state, setstate] = useState([])
+
+
+
+    const getdata = async () => {
+
+
+        const resp = await fetch("https://api.covid19india.org/data.json")
+        const data = await resp.json()
+        setstate(data.statewise);
+        console.log(data.statewise);
+
+    }
+
+
+    useEffect(() => {
+        getdata()
+
+    }, [])
+
+    return (
+        <div id="container">
+
+
+
+            <div id="center">
+                <h1 id="heading"> INDIA COVID-19 Dashboard </h1>
+
+                <table id="content-table">
+                    <thead>
+                        <tr>
+                            <th id="m">STATE</th>
+                            <th>CONFIRMED</th>
+                            <th>RECOVERED</th>
+                            <th>DEATH</th>
+                            <th>ACTIVE</th>
+                            <th>UPDATED</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+
+                        {state.map((value, index) => {
+
+                            const { state, confirmed, recovered, deaths, active, lastupdatedtime } = value;
+
+                            return <tr key={index}>
+
+                                <td>{state}</td>
+                                <td>{confirmed}</td>
+                                <td>{recovered}</td>
+                                <td>{deaths}</td>
+                                <td>{active}</td>
+                                <td>{lastupdatedtime}</td>
+                            </tr>
+                        })
+
+                        }
+                    </tbody>
+
+                </table>
+
+
+            </div>
+        </div>
+
+    )
 }
-
 export default App;
